39d6a112c30408dfca0b76e4ccd603f0134a7a21
Axel Huebl <axel.huebl@plasma.ninja>
2019-01-05 00:57:39 +0100

    bump build number

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,16 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     # remove -lrt
-    sed -i '.bak' 's/ -lrt//g' $SRC_DIR/wrappers/numpy/Makefile
+    sed -i '.bak' 's/ -lrt//g' ${SRC_DIR}/wrappers/numpy/Makefile
 fi
 
 # Python3 fixes
 if [[ "${PY_VER}" =~ 3 ]]
 then
-  find $SRC_DIR/utils -name "*.py" -exec 2to3 -w -n {} \;
+  find ${SRC_DIR}/utils -name "*.py" -exec 2to3 -w -n {} \;
 fi
 
 # numpy bindings
 cd wrappers/numpy
 make python
-$PYTHON setup.py install
+${PYTHON} setup.py install
